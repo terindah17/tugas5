a68cc34a5adcad89d28808d9a3267dee3c6706d4
terindahpassaramadhani@gmail.com <terindahpassaramadhani@gmail.com>
2025-08-25 12:56:04 +0700

    lingkaran

---
+++ luas-lingakaran.php
@@ -0,0 +1,15 @@
+<?php
+// Jari-jari lingkaran
+$r = 7;
+
+// Rumus luas lingkaran
+$luas = M_PI * ($r ** 2);
+
+// Rumus keliling lingkaran
+$keliling = 2 * M_PI * $r;
+
+// Output hasil
+echo "Jari-jari = $r <br>";
+echo "Luas Lingkaran = $luas <br>";
+echo "Keliling Lingkaran = $keliling";
+?>
